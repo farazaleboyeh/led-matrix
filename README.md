b0b8c1d3c42a8f69943e88dd209f3d63f02e31bb
Faraz.Aleboyeh <faraz.aleboyeh@forces.gc.ca>
2026-08-01 14:58:22 -0400

    Commit #4

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-hi test
+### Phase I
+- Single LED activation
+- Verify row/column mapping
+- Row scanning implementation
+
+### Phase II
+- Hardware timer refresh
+- Monochrome framebuffer
+- setPixel() 
+
+### Phase III
+- Lines, rectangles, circles
+- AxB font
+- Text scrolling
+
+### Phase IV
+- PWM/BCM grayscale
+- Performance optimization with SPI + DMA
+
+### Phase V
+- Resolution-independant graphics layer
+- Image decoding
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "master.h"
 
-
 const int latchp = 15;
 const int clockp = 17;
 const int datap = 7;
@@ -22,6 +21,17 @@ void shiftLED(uint16_t thisLED)
 
 void setLED()
 {
+  LED = 0b0000000000111111;
+  shiftLED(LED);
+  delay(200);
+
+  LED = 0b0100100101011111;
+  shiftLED(LED);
+  delay(200);
+
+  LED = 0b1101101101101111;
+  shiftLED(LED);
+  delay(200);
 
 }
 
@@ -30,13 +40,15 @@ void setup()
   Serial.begin(115200);
 
   rgb_init();
-  rgb_set_color("white");
+  rgb_set_color("red");
 
- 
-
+  pinMode(latchp, OUTPUT);
+  pinMode(clockp, OUTPUT);
+  pinMode(datap, OUTPUT);
 }
 
 void loop()
 {
+  setLED();
 
 }
